b09ed2c42eea844a5e6684f4f277f01aec503635
rigolook <rixxormen@gmail.com>
2023-10-25 11:04:49 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicios_ERIC/Program.cs
@@ -0,0 +1,38 @@
+﻿    namespace Ejercicios_ERIC
+    {
+        using System;
+
+        class Program
+        {
+            static void Main()
+            {
+                Ejercicios ejercicios = new Ejercicios();
+
+                // Ejercicio 1
+                int[] arreglo1 = { 1, 2, 3, 4, 5 };
+                ejercicios.MostrarArreglo(arreglo1);
+
+                // Ejercicio 2
+                int[] arreglo2 = ejercicios.LeerArreglo();
+                ejercicios.MostrarArreglo(arreglo2);
+
+                // Ejercicio 3
+                int[] arreglo3 = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+                double media = ejercicios.CalcularMedia(arreglo3);
+                Console.WriteLine("La media de los valores del arreglo es: " + media);
+
+                // Ejercicio 4
+                int tamaño = ejercicios.LeerTamaño();
+                int numero = ejercicios.LeerNumero();
+                int[] arreglo4 = ejercicios.CrearArregloMultiplos(tamaño, numero);
+                ejercicios.MostrarArreglo(arreglo4);
+
+                // Ejercicio 5
+                string[] nombres = ejercicios.LeerNombres(tamaño);
+                int[] longitudes = ejercicios.CalcularLongitudes(nombres);
+                ejercicios.MostrarNombresYLongitudes(nombres, longitudes);
+            }
+        }
+
+   
+    }
---
+++ Ejercicios_ERIC/ejercicios.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace Ejercicios_ERIC
+    {
+    
+            class Ejercicios
+            {
+                public void MostrarArreglo(int[] arreglo)
+                {
+                    Console.WriteLine("Elementos del arreglo:");
+                    foreach (int num in arreglo)
+                    {
+                        Console.Write(num + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                public int[] LeerArreglo()
+                {
+                    int[] arreglo = new int[5];
+                    Console.WriteLine("Introduce 5 valores para el arreglo:");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        arreglo[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    return arreglo;
+                }
+
+                public double CalcularMedia(int[] arreglo)
+                {
+                    int suma = 0;
+                    foreach (int num in arreglo)
+                    {
+                        suma += num;
+                    }
+                    return (double)suma / arreglo.Length;
+                }
+
+                public int LeerTamaño()
+                {
+                    Console.WriteLine("Introduce el tamaño del arreglo:");
+                    return Convert.ToInt32(Console.ReadLine());
+                }
+
+                public int LeerNumero()
+                {
+                    Console.WriteLine("Introduce el número para calcular los múltiplos:");
+                    return Convert.ToInt32(Console.ReadLine());
+                }
+
+                public int[] CrearArregloMultiplos(int tamaño, int numero)
+                {
+                    int[] arreglo = new int[tamaño];
+                    for (int i = 0; i < tamaño; i++)
+                    {
+                        arreglo[i] = (i + 1) * numero;
+                    }
+                    return arreglo;
+                }
+
+                public string[] LeerNombres(int tamaño)
+                {
+                    string[] nombres = new string[tamaño];
+                    Console.WriteLine("Introduce los nombres:");
+                    for (int i = 0; i < tamaño; i++)
+                    {
+                        nombres[i] = Console.ReadLine();
+                    }
+                    return nombres;
+                }
+
+                public int[] CalcularLongitudes(string[] nombres)
+                {
+                    int[] longitudes = new int[nombres.Length];
+                    for (int i = 0; i < nombres.Length; i++)
+                    {
+                        longitudes[i] = nombres[i].Length;
+                    }
+                    return longitudes;
+                }
+
+                public void MostrarNombresYLongitudes(string[] nombres, int[] longitudes)
+                {
+                    Console.WriteLine("Nombres y sus longitudes:");
+                    for (int i = 0; i < nombres.Length; i++)
+                    {
+                        Console.WriteLine(nombres[i] + ": " + longitudes[i]);
+                    }
+                }
+            }
+    
+    }
